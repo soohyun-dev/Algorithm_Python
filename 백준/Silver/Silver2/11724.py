9fecbc3c52469c9f6b28990ef574d9c68f3302a6
soohyun <bmy1320@gmail.com>
2022-02-15 23:33:38 +0900

    2022-02-15

--- 백준/Silver/Silver2/11724.py
+++ 백준/Silver/Silver2/11724.py
@@ -1,27 +1,27 @@
 import sys
+sys.setrecursionlimit(100000)
 input=sys.stdin.readline
 
-def dfs(u):
-    visited[u]=True
-    for i in graph[u]:
+def dfs(start):
+    visited[start]=True
+    for i in graph[start]:
         if not visited[i]:
             dfs(i)
 
 N,M=map(int,input().split())
 graph=[[] for _ in range(N+1)]
+visited=[False]*(N+1)
+cnt=0
 
 for _ in range(M):
     u,v=map(int,input().split())
     graph[u].append(v)
     graph[v].append(u)
 
-visited=[False]*(N+1)
-cnt=0
-
 for i in range(1,N+1):
     if visited[i]==False:
-        cnt+=1
         dfs(i)
+        cnt+=1
 
 print(cnt)
     
---
+++ 백준/Silver/Silver5/11170.py
@@ -0,0 +1,10 @@
+for i in range(int(input())):
+    N,M=(map(int,input().split()))
+    cnt=0
+    for i in range(N,M+1):
+        l=list(str(i))
+        for j in range(len(l)):
+            if l[j]=='0':
+                cnt+=1
+
+    print(cnt)
