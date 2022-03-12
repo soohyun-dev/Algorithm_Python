0b868d8f9d275a16d2cc27c753af93f6a6b54e61
soohyun <bmy1320@gmail.com>
2022-03-12 21:46:10 +0900

    2022-03-12

---
+++ 백준/Silver/Silver1/2156.py
@@ -0,0 +1,18 @@
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+dp=[0 for i in range(n+1)]
+wine=[0]
+for i in range(n):
+    wine.append(int(input()))
+dp[1]=wine[1]
+if n>1:
+    dp[2]=sum(wine[:3])
+    for i in range(3,n+1):
+        dp[i]=max(dp[i-1],dp[i-3]+wine[i-1]+wine[i],dp[i-2]+wine[i])
+print(dp)
+print(dp[-1])
+
+        
+        
