45606df39b48ab91e3ffb9f59f3663905df69671
soohyun <bmy1320@gmail.com>
2022-03-14 18:50:56 +0900

    2022-03-14

---
+++ 백준/Silver/Silver2/15990.py
@@ -0,0 +1,8 @@
+import sys
+input=sys.stdin.readline
+
+dp=[0]*100001
+
+for i in range(int(input())):
+    print(dp[int(input())])
+
--- 백준/Silver/Silver5/2535.py
+++ 백준/Silver/Silver5/2535.py
@@ -1,5 +1,9 @@
+import sys
+input=sys.stdin.readline
+
 l=[]
-for i in range(int(input())):
+n=int(input())
+for i in range(n):
   l.append(list(map(int, input().split())))
   l.sort(key=lambda x:-x[2])
 
@@ -7,6 +11,9 @@ print(*l[0][:2])
 print(*l[1][:2])
 
 if l[0][0]==l[1][0]:
-  print(*l[3][:2])
+  for i in range(2,n):
+      if l[i][0]!=l[1][0]:
+          print(*l[i][:2])
+          break
 else:
   print(*l[2][:2])
