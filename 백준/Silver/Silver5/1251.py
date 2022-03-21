32661b180744e8d7ed306869fce8b0b34ccce900
soohyun <bmy1320@gmail.com>
2022-03-21 20:19:48 +0900

    2022-03-21

---
+++ 백준/Silver/Silver5/1251.py
@@ -0,0 +1,16 @@
+wool=list(map(int,input().split()))
+start=list(map(int,input().split()))
+wool_cnt=0
+start_cnt=0
+check=False
+
+for i in range(9):
+    wool_cnt+=wool[i]
+    if wool_cnt>start_cnt:
+        check=True
+    start_cnt+=start[i]
+
+if check==True:
+    print('Yes')
+else:
+    print('No')
