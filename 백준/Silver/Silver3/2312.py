6c2a070abc395a92887ddd4b0490a6d0cb23e04b
soohyun <bmy1320@gmail.com>
2022-03-24 15:42:47 +0900

    2022-03-24

---
+++ 백준/Silver/Silver3/2312.py
@@ -0,0 +1,21 @@
+import sys
+input=sys.stdin.readline
+
+N=int(input())
+for i in range(N):
+    num=int(input())
+    a=2
+    cnt=[]
+    while a<=num:
+        if num%a==0:
+            num//=a
+            cnt.append(a)
+        else:
+            a+=1
+    cnt.sort()
+    tmp=set(cnt)
+    value=sorted(list(tmp))
+
+    for i in range(len(value)):
+        print(value[i],cnt.count(value[i]))
+     
