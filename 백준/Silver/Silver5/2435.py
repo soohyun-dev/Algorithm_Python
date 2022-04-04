c96230b30e1040b96ca694bbc77bf662f9bdc6f7
soohyun <bmy1320@gmail.com>
2022-04-04 10:55:58 +0900

    2022-04-04

--- 백준/Silver/Silver5/2435.py
+++ 백준/Silver/Silver5/2435.py
@@ -1,7 +1,9 @@
 N,K=map(int,input().split())
-temper=list(map(int,input().split()))
+tem=list(map(int,input().split()))
 top=-100
 for i in range(N-K+1):
-    top=max(sum(temper[i:K+i]),top)
+    top=max(sum(tem[i:K+i]),top)
 
-    print(top)
+print(top)
+
+
