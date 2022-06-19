16a3a67be1dc8667c82394279e9b7484b61f2c9f
soohyun <bmy1320@gmail.com>
2022-06-19 21:22:38 +0900

    2022-06-19

---
+++ 백준/Bronze/Bronze3/11109.py
@@ -0,0 +1,14 @@
+import sys
+input=sys.stdin.readline
+
+for _ in range(int(input())):
+    d,n,s,p=map(int,input().split())
+    A=n*s
+    B=d+n*p
+    if A>B:
+        print('do not parallelize')
+    elif A<B:
+        print('parallelize')
+    else:
+        print('does not matter')
+        
