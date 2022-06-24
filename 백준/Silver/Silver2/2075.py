31cce4bc4466a801cdd2b009778607b533dc453c
soohyun <bmy1320@gmail.com>
2022-06-24 14:56:44 +0900

    2022-06-24

---
+++ 백준/Silver/Silver2/2075.py
@@ -0,0 +1,17 @@
+import heapq
+import sys
+input=sys.stdin.readline
+
+N=int(input())
+heap=[]
+heapq.heapify(heap)
+
+for _ in range(N):
+    tmp=list(map(int,input().split()))
+    for i in range(N):
+        heapq.heappush(heap, tmp[i])
+        if len(heap)>N:
+            heapq.heappop(heap)
+print(heap)
+print(heap[0])
+
