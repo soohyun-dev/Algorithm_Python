323f2ebfa2b41cd93eebcd4c8537429b3d36b4cc
soohyun <bmy1320@gmail.com>
2022-07-07 11:44:24 +0900

    2022-07-07

---
+++ 백준/Silver/Silver4/1051.py
@@ -0,0 +1,23 @@
+N,M=map(int,input().split())
+l=[]
+cnt=N-1
+for i in range(N):
+    l.append(list(map(int,input())))
+    
+check=False
+
+while True:
+    for i in range(N-cnt):
+        for j in range(M-cnt):
+            if l[i][j]==l[i][j+cnt]==l[i+cnt][j]==l[i+cnt][j+cnt]:
+                print((cnt+1)*(cnt+1))
+                check=True
+                break
+        if check==True:
+            break
+    cnt-=1
+    if check==True or cnt==0:
+        break
+
+if check==False:
+    print(1)
