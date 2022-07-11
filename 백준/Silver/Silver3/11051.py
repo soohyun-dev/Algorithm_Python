c921084db44b3f3d990052aed117a4d45cad7480
soohyun <bmy1320@gmail.com>
2022-07-11 15:35:53 +0900

    2022-07-11

---
+++ 백준/Silver/Silver3/11051.py
@@ -0,0 +1,20 @@
+n,k=map(int,input().split())
+
+tmp=n-k
+down=1
+up=1
+
+if k>=tmp:
+    a=k
+    b=tmp
+else:
+    a=tmp
+    b=k 
+    
+for i in range(2,a+1):
+    down*=i
+for j in range(b+1,n+1):
+    up*=j
+    
+result=(up//down)%10007
+print(result)
