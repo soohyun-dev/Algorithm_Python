fed2e444191cea5910e854f4a8f96f51fd1d0bd3
soohyun <bmy1320@gmail.com>
2022-07-13 15:41:43 +0900

    2022-07-13

---
+++ 백준/Silver/Silver4/4949.py
@@ -0,0 +1,35 @@
+while True:
+    l=list((input()))
+    check=[]
+    if l[0]=='.' and len(l)==1:
+        break
+    else:
+        for i in range(len(l)):
+            if l[i]=='(':
+                check.append('(')
+            elif l[i]=='[':
+                check.append('[')
+            elif l[i]==')':
+                if '(' in check:
+                    if check[-1]=='(':
+                        del(check[-1])
+                    else:
+                        break
+                else:
+                    check.append('no')
+                    break
+            elif l[i]==']':
+                if '[' in check:
+                    if check[-1]=='[':
+                        del(check[-1])
+                    else:
+                        break
+                else:
+                    check.append('no')
+                    break
+                
+    if len(check)==0:
+        print('yes')
+    else:
+        print('no')
+            
